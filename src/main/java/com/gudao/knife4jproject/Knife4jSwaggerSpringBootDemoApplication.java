7cb95b464b2b3427b3552e0643e741ce8a4f690c
gudaofq <2873908996@qq.com>
2021-06-02 13:30:37 +0800

    add fix: 项目基础提交（lombok+springboot）

---
+++ src/main/java/com/gudao/knife4jproject/Knife4jSwaggerSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gudao.knife4jproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Knife4jSwaggerSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Knife4jSwaggerSpringBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gudao/knife4jproject/Knife4jSwaggerSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gudao.knife4jproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Knife4jSwaggerSpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
